73182df3eae65c277c9ca42ecafc695c1c52e04d
Gustavo Costa <gustavocosta.franco.almeida@gmail.com>
2025-04-11 13:19:57 -0300

    arrumando os dados

--- server/api/criar_ideia.js
+++ server/api/criar_ideia.js
@@ -2,17 +2,31 @@ import mongoose from "mongoose";
 import Ideia from "../models/Ideia";
 
 export default defineEventHandler(async (event) => {
-    try {
-        const body = await readBody(event);
+  try {
+    const config = useRuntimeConfig()
 
-        const nova_ideia = await Ideia.create(body.dados)
-        return {
-            statusCode : 200,
-            success: true,
-            message: "IDEIA ADICIONADA COM SUCESSO! EM BREVE ENTRAREMOS EM CONTATO!",
-            nova_ideia,
-          };        
-    } catch (erro) {
-        console.log(`Erro ao processar a ideia: ${erro}`);
+    // Conecta com o Mongo usando a variável MONGO_URI do Railway
+    if (mongoose.connection.readyState === 0) {
+      await mongoose.connect(config.MONGO_URI)
     }
-});
+
+    const body = await readBody(event)
+
+    const nova_ideia = await Ideia.create(body.dados)
+
+    return {
+      statusCode: 200,
+      success: true,
+      message: "IDEIA ADICIONADA COM SUCESSO! EM BREVE ENTRAREMOS EM CONTATO!",
+      nova_ideia,
+    }
+  } catch (erro) {
+    console.error(`Erro ao processar a ideia: ${erro}`)
+    return {
+      statusCode: 500,
+      success: false,
+      message: "Erro ao processar a ideia",
+      erro,
+    }
+  }
+})
