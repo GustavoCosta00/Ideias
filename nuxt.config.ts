d770ede87e78e0c782522a8cb14dd2c7c6e33389
Gustavo Costa <gustavocosta.franco.almeida@gmail.com>
2025-04-11 13:14:51 -0300

    a

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: "2025-04-11",
   runtimeConfig: {
-    MONGO_URI: process.env.MONGO_URI,
+    MONGO_URI: process.env.MONGO_URI || "mongodb+srv://gustavo:gustavinMarmitinha@ideias.2wnnsyo.mongodb.net/?retryWrites=true&w=majority&appName=Ideias"
   }
 });
